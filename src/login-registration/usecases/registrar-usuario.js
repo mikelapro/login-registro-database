44166fcbf402c0e55294082b012e7830d8acfd12
Mike <mikecsomma@gmail.com>
2023-11-18 14:29:45 -0300

    Se habilitó CORS.

--- src/login-registration/usecases/registrar-usuario.js
+++ src/login-registration/usecases/registrar-usuario.js
@@ -1,20 +1,21 @@
 import{ config } from '../models/config.js';
 
 export const registrarUsuario = async ( usuario ) => {
-    // FIXED: Faltaba CORS y agregar /usuarios al uri.
+    // *FIXED: Faltaba CORS y agregar /usuarios al uri.
     // TODO: Hay que devolver el reques pero con el usuario registrado, si no en el 
     // la línea 90 de register.js al querer leer el .nombre, da error.
-    // OJO: hay un response pero es raro, hay que convertirlo al usuario registrado.
+    // !OJO: hay un response pero es raro, hay que convertirlo al usuario registrado.
+
 
     let response = null;
 
     const endpoint = `${config.apiBaseUri}/usuarios`;
+    const body = JSON.stringify( usuario );
 
     try {
         response = await fetch( endpoint, {
             method: 'POST',
-            body: JSON.stringify( usuario ),
-            mode: 'cors',
+            body: body,
             headers: {
                 'Content-Type': 'application/json'
             }
