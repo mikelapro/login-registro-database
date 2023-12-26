0c9e691df5d588e1ad74d9e384da59797dbeaa11
Mike <mikecsomma@gmail.com>
2023-12-26 19:27:01 -0300

    Cualquier cosa.

--- src/login-registration/pages/main.js
+++ src/login-registration/pages/main.js
@@ -1,4 +1,5 @@
 import { checkApp } from '../usecases/check-app.js';
+import{ config } from '../models/config.js';
 
 checkApp();
 
@@ -13,3 +14,41 @@ function getNombreFromQueryString() {
 
 let h1NombreUsuario = document.getElementById( 'h1-bienvenida' );
 h1NombreUsuario.innerHTML = 'Bienvenido ' + getNombreFromQueryString() + '!';
+
+
+const prueba = async () => {
+    // Obtiene el accessToken del local storage.
+    let accessToken = localStorage.getItem( 'access-token' );
+    console.log( accessToken );
+
+    // 
+    if ( accessToken == null || accessToken == undefined ) {
+        window.location.href = 'login.html';
+    }
+
+
+    let response;
+    let objResponse;
+    const endpoint = `${config.apiBaseUri}/usuarios/auth`;
+    //const body = JSON.stringify( usuario );
+
+    // Hit al API.
+    response = await fetch( endpoint, {
+        method: 'GET',
+        //body: body,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': accessToken
+        }
+    } );
+
+    // Convierte el json del response en objeto.
+    objResponse = await response.json();
+    console.log( objResponse );
+};
+
+prueba();
+
+
+
+// Obtiene el usuario autenticado (el usuario correspondiente al accessToken).
