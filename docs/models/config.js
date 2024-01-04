15febf2671d68c6da3c28957846bb72a02a17a40
Mike <mikecsomma@gmail.com>
2024-01-04 19:56:03 -0300

    Se sacó el número de puerto de la URI del api para producción.

--- docs/models/config.js
+++ docs/models/config.js
@@ -1,5 +1,5 @@
 export const config = {
     // apiBaseUri: 'Http://localhost:3000'
-    apiBaseUri: 'https://login-registro-database-api.onrender.com:10000'
+    apiBaseUri: 'https://login-registro-database-api.onrender.com'
 };
 
